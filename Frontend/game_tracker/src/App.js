423159b6e30a7d8dea0f4a8ccdc2ebfddf0ec029
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-07-01 18:44:35 -0400

    New push

--- Frontend/game_tracker/src/App.js
+++ Frontend/game_tracker/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Game Tracker
-        </a>
-      </header>
-    </div>
+      <Router>
+            <header>
+                <div className="navBar">
+                    <ul>
+                      <li>
+                      <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                      <Link to="/about">About Us</Link>
+                      </li>
+                      <li>
+                      <Link to="/packages">Our Packages</Link>
+                      </li>
+                    </ul>
+                </div>
+            </header>
+        
+        <div className="display">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/packages" element={<Packages />} />
+            </Routes>
+        </div>
+        
+        
+        </Router>
+      </div>
   );
 }
 
---
+++ Frontend/game_tracker/src/components/index.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div className="aboutpage">
+            <div className="aboutHeader">
+                <h1></h1>
+            </div>
+        </div>
+    )
+}
