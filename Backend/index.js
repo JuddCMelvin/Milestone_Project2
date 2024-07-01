4710ecbb4cb0a83d7954080da5789d05629ba18d
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-01 16:53:31 -0400

    stub routes added, connected to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// middleware to parse JSON bodies //
+app.use(express.json());
 
-app.use('/games', require('./controllers/games')); // gets games model //
+// routes //
+app.use('/games', require('./controllers/games')); // route for game-related operations //
 
+// home page or default landing //
 app.get('/', (req, res) => {
-    res.send('Hello Team!')
-}); // home-default page  //
+    res.send('Hello Team!');
+});
 
+// 404 page for undefined routes //
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page Not Found</h1>')
-}); // 404 page // 
+    res.status(404).send('<h1>404 Page Not Found</h1>');
+});
 
-app.listen(process.env.PORT);
+// error handling middleware /
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke -_- !');
+});
+
+// start the server //
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
