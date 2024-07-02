984f175ba6849cd9e6c6f9a8c760c10e47c36710
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-07-02 19:42:00 -0400

    got react router dom connected

--- Frontend/game_tracker/src/App.js
+++ Frontend/game_tracker/src/App.js
@@ -1,15 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import TopBar from './components/TopBar'
-import Test from './components/Test'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import Games from './components/MyGames'
+import Home from './components/Home'
+
 
 function App() {
+  
+  // const games = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+
+
   return (
     <div className="App">
-      <TopBar/>
-      <Test /> 
+      <Router>
+        <header>
+          <h1 className="title">GameTracker</h1>
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Games">Games</Link>
+              </li>
+              <li>
+                <Link to="/MyGames">MyGames</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <div className="display">
+          <Routes>
+            <Route path="/" component={Home} />
+            <Route path="/games" component={Games} />
+            {/* <Route path="/packages" render={() => <MyGames games={games}/>}  /> */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
--- Frontend/game_tracker/src/components/Test.js
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-    return (
-        <div style={{display: "flex"}}>
-            <h1>test2</h1>
-        </div>
-    )
-}
