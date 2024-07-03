ec1e17001d570106314dca93cea28ba96d471b24
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-07-03 19:39:17 -0400

    added search bar

--- Frontend/game_tracker/src/App.js
+++ Frontend/game_tracker/src/App.js
@@ -3,11 +3,39 @@ import logo from './logo.svg'
 import './App.css'
 import Games from './components/MyGames'
 import Home from './components/Home'
-
-
+import { Fragment } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  let [search, setSearch] = useState('')
+	let [message, setMessage] = useState('Search for Games')
+	let [data, setData] = useState([])
+
+	const API_URL = ' http://localhost:3004/games/search?query='
+
+	useEffect(() => {
+		if(search) {
+			const fetchData = async () => {
+				document.title = `${search} Games`
+				const response = await fetch(API_URL + search)
+				const resData = await response.json()
+				if (resData.results.length > 0) {
+					return setData(resData.results)
+				} else {
+					return setMessage('Not Found')
+				}
+			}
+			fetchData()
+		}
+	}, [search])
+	
+	const handleSearch = (e, term) => {
+		e.preventDefault()
+		setSearch(term)
+	}
 
   return (
     <div className="App">
@@ -31,7 +59,12 @@ function App() {
         <div className="display">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/games" element={<Games />} />
+            <Route path="/games" element={
+              <Fragment>
+                <SearchBar handleSearch = {handleSearch}/>
+							  <Gallery data={data} />
+              </Fragment>
+            } />
             {/* <Route path="/packages" render={() => <MyGames games={games}/>}  /> */}
           </Routes>
         </div>
