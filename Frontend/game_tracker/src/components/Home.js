9b969ec21c40fd4dec85941e6248cfa14e4fee2e
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-07-03 15:36:26 -0400

    udpate

---
+++ Frontend/game_tracker/src/components/Home.js
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+    return(
+        <div className="homepage">
+            <h1>PlaceHolder</h1>
+            
+            <h1>Welcome to our app</h1>
+
+        </div>
+    )
+
+}
