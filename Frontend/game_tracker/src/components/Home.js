e74cc34bb25542b18d199d0e1b31fb9a8607bdc0
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-07-04 04:35:34 -0400

    switching to no api

--- Frontend/game_tracker/src/components/Home.js
+++ Frontend/game_tracker/src/components/Home.js
@@ -4,9 +4,8 @@ export default function Home() {
     return(
         <div className="homepage">
             <h1>PlaceHolder</h1>
-            
+             
             <h1>Welcome to our app</h1>
-
         </div>
     )
 
