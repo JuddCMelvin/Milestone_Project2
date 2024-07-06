52bcee74f3a5efe867a390fc0c2a9570cb2400f4
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-06 11:37:35 -0400

    naming consistency

--- Backend/models/review.js
+++ Backend/models/review.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // review schema //
 const ReviewSchema = new mongoose.Schema({
-    game: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true }, // reference to the game being reviewed //
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // reference to the user who wrote the review //
+    title: { type: mongoose.Schema.Types.ObjectId, ref: 'Title', required: true }, // reference to the game being reviewed //
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'author', required: true }, // reference to the user who wrote the review //
     review: { type: String, required: true }, // content of the review //
     rating: { type: Number, min: 1, max: 5, required: true }, // rating given by the user (1 to 5) //
     createdAt: { type: Date, default: Date.now } // date when the review was created //
