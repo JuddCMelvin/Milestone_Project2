9a62861b1316c3e4b7a17bcba1f462eb2dbb3575
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-07 20:31:34 -0400

    home page with button

--- Frontend/game_tracker/src/Home.js
+++ Frontend/game_tracker/src/Home.js
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
-import logo from './logo.svg'
+import React from 'react';
+import { Link } from 'react-router-dom'; // imports Link from react-router-dom //
+import './App.css';
 
 function Home() {
     return (
-        <main>
-            <h1 className="homeHeader">HOME</h1>
-            <div>
-                <img height="300" width="500" src="http://localhost:5000/images/gamecontroller.jpeg" alt="Chia Fruit Shake" />
-                <p>
-                    Welcome to Your Game Journal!
-                    Track your progress, rate your games, 
-                    and discover new adventures. Stay on top of your 
-                    gaming life with Game Tracker.
-                </p>
-            </div>
-        </main>
-    );
-}
+        <div>
+            <main>
+                <h1>WELCOME!</h1>
+                <div>
+                    <Link to="/games">
+                        <img src="/gameZone.png" alt="game zone sign" height="300" width="300" />
+                    </Link>
+                    <p>
+                        Track your progress, rate your favorite games, and revisit your gaming highlights. Level up your gaming experience and stay in control of your gaming life with Game Zone!
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+};
 
 export default Home;
