8e051ad33019209b78c76b56cb4bdb47746fa25a
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-07 21:07:22 -0400

    removed unneeded imports and navbar,(own component now)

--- Frontend/game_tracker/src/App.js
+++ Frontend/game_tracker/src/App.js
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
-import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Home'
 import Index from './components/games/gamesIndex.js'
 import NewGame from './components/games/NewGame.js'
 import EditGameForm from './components/games/EditGameForm.js'
 import NewReviewForm from './components/games/NewReview.js'
-import Error404 from './Error404'
-import { Fragment } from 'react'
-import { useState, useEffect } from 'react'
+import EditReview from './components/games/EditReview.js'
 import GameDetails from './components/games/GameDetails.js'
 
 function App() {
@@ -16,19 +13,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/games">Games</Link>
-                </li>
-                <li>
-                    <Link to="/games/new">Add New Game</Link>
-                </li>
-            </ul>
-        </div>
         <div className="display">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -37,12 +21,13 @@ function App() {
             <Route exact path="/games/:gameId" element={<GameDetails />} />
             <Route exact path="/games/:gameId/edit" element={<EditGameForm />} />
             <Route exact path="/games/:gameId/review" element={<NewReviewForm />} />
+            <Route exact path="/games/:gameId/review/:reviewId/edit" element={<EditReview />} />
           </Routes>
         </div>
       </Router>
     </div>
-  );
-}
+  )
+};
 
 export default App;
 
